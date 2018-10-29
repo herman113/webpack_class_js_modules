9cc7eb0d85b29231b742eae1d7dee52958fe2e9c
Herman Soto <hermansoto@me.com>
2018-10-29 10:27:19 -0400

    Update index.js add button with vanilla js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-// const sum = require('./sum');
-import sum from './sum';
-import './image_viewer';
+const button = document.createElement('button');
+button.innerText = 'Click me';
+button.onclick = () => {
 
-const total = sum(10, 5);
-console.log(total);
+};
+
+document.body.appendChild(button);
