90529f53d2aae775c88e9567b7cab2b29bf282e9
Herman Soto <hermansoto@me.com>
2018-10-29 10:45:09 -0400

    Update index.js image_viewer.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const button = document.createElement('button');
 button.innerText = 'Click me';
 button.onclick = () => {
-
+  System.import('./image_viewer').then(module => {
+    console.log(module);
+    module.default();
+  });
 };
 
 document.body.appendChild(button);
